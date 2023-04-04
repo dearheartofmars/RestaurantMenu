9f2528f3c343e7d5f097c2a2a795b413b26d3232
dearheartofmars <mnlebeau@gmail.com>
2023-04-03 19:27:53 -0500

    Classes studio part 1

---
+++ src/main/java/Restaurant/Menu.java
@@ -0,0 +1,9 @@
+package Restaurant;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Menu {
+    ArrayList<String> menu = new ArrayList<>();
+    private Date LastUpdated = new Date;
+}
